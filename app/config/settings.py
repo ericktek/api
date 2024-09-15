4d73f5afc2a4d758892687ab9ad5db963a940d59
ericktek <erickbale360@gmail.com>
2024-09-15 11:00:38 +0300

    refactoring code

---
+++ app/config/settings.py
@@ -0,0 +1,10 @@
+import os
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "your_secret_key")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,19 +1,35 @@
 from fastapi import FastAPI
-from app.config.database import engine, Base
-from app.routers import auth_router, course_router, online_class_router
-
-app = FastAPI()
-
-# Create the database tables
-Base.metadata.create_all(bind=engine)
-
-# Health check
-@app.get("/", tags=["Health"])
-async def read_root():
-    return {"message": "Server is healthy"}
+from fastapi.responses import HTMLResponse
 
 
-# Register the routers
-app.include_router(auth_router.router, prefix="/auth")
-app.include_router(course_router.router, prefix="/courses")
-app.include_router(online_class_router.router, prefix="/online-classes")
+app = FastAPI(
+    title="LMS API",
+    description="A simple and modern learning management system backend built with FastAPI and PostgreSQL",
+    version="1.0.0"
+)
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LMS</title>
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            <h2>Modern Learning Management System [LMS backend]!</h2>
+            <ul>
+                <li><a href="/docs">/docs</a></li>
+                <li><a href="/redoc">/redoc</a></li>
+            </ul>
+           <p>Built and deployed by Lemajr <a href="https://github.com/ericktek" target="_blank">GitHub</a>
+                    Powered by <a href="https://fastapi.tiangolo.com" target="_blank">FastAPI</a> and <a href="https://vercel.com" target="_blank">Vercel</a></p>
+        </div>
+    </body>
+</html>
+
+"""
+
+@app.get("/", response_class=HTMLResponse, tags=["Health"])
+async def root():
+    return html
